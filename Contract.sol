e0fa31a27e833a172f61772b49f693cc04d65768
Gnanababu <123804216+Gnanababu@users.noreply.github.com>
2023-03-28 09:28:06 +0530

    Create Contract.sol

---
+++ Contract.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: 
+MITj pragma solidity >=0.4.22 <0.9.0;
+added the one more line added the line number 4 and demo added the line number 4 and demo added the line number 5 and demo 
+contract Owner{     
+modifier onlyOwner(){        
+added one more new line extras for 5        
+added new line extras for 5        
+added line extra 5        
+added line 9 and updated     
+added line 5       
+require(msg.sender == owner);      
+added the line new number 10        
+require(msg.messanger == sender);  //dummy text    
+require(msg.messanger == sender);  //dummy text         _;     } } 
+contract MyContract is Owner {     mapping(address => uint) balances;  
+mapping(address => uint) total;   //dummy text   
+function mint(uint value) onlyOwner public{     
+balances[msg.sender] += value;   
+hello i am the test         
+test 21   
+mapping(address => uint) total;   
+//dummy text dummy text   }
+hello line 25 test 27 added
+added 28 line added added line 30
+added line 31 added line 32 added the new line 
